e2f56ac1d186f6e1d48b17a670c7691a74ef65a2
Spéro ADONON <spro.adonon96@gmail.com>
2021-09-28 09:23:13 +0100

    Add last year decompte

--- 2k22_decompt.py
+++ 2k22_decompt.py
@@ -1,17 +1,11 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Desc: Obtenez les dernières statistiques du Bénin sur le coronavirus
-# Author: https://twitter.com/SperoAdonon
-#
+from datetime import date
 from dotenv import dotenv_values
-from covid_stat import get_gouv_bj_covid_stats
 import tweepy as tp
-import time
 import logging
 
 logging.basicConfig(level=logging.INFO, filename="bot_log.txt",
                     format='%(asctime)s :: %(levelname)s :: %(message)s')
+
 config = dict(dotenv_values(".env"))
 
 # Connect to twitter api
@@ -20,12 +14,16 @@ auth.set_access_token(config['ACCESS_TOKEN'], config['ACCESS_TOKEN_KEY'])
 
 api = tp.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-status_message = get_gouv_bj_covid_stats()
+today = date.today()
+last_day_of_year = date(2021,12,31)
 
-# update twitter status
+delta = last_day_of_year - today
+
+message = f"Il reste {delta.days} jours avant la fin de l'année."
+
 try:
     logging.info("Updating status")
-    api.update_status(status_message)
+    api.update_status(message)
     logging.info("End updating status")
 except tp.TweepError as error:
     if error.api_code == 187:
